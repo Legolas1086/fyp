c3dffa5771a55cf6698874a03d51a3323ee20b0a
Sumukh <sumukhbale@gmail.com>
2022-07-08 23:57:52 +0530

    login broken

--- frontend/src/cred.js
+++ frontend/src/cred.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react";
-
-import login from "./images/login.svg";
 import drawing from "./images/drawing.svg";
 import styles from "./css/login.css";
 import Login from "./login.js"
@@ -12,9 +10,6 @@ const Credential=()=>{
         let navigation = useNavigate()
         return(
         <div className="OuterContainer">
-                <div className="imgs">
-                    <img className="image-login" src={login}/>
-                </div>
             <div className="limiter">
             <div className="container-login">
             <div className="wrap-login">
--- frontend/src/registerUser.js
+++ frontend/src/registerUser.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react";
-
-import login from "./images/login.svg";
 import drawing from "./images/drawing.svg";
 import styles from "./css/login.css";
 import Register from "./register"
@@ -11,8 +9,18 @@ class RegisterUser extends React.Component{
     render(){
         return(
         <div className="OuterContainer">
+            <div className="limiter">
+            <div className="container-login">
+            <div className="wrap-login">
                <Register/>
-               </div>
+                <div className="login100-more">
+                </div>
+            </div>
+         </div>
+         </div>
+
+    
+            </div>
             
 
         );
