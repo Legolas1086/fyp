365dfe43d800d24a3699264e2c8beb05d17a9c5f
Legolas1086 <sanjayurs16@gmail.com>
2022-07-10 21:14:46 +0530

    everything working in branch

--- backend/home/urls.py
+++ backend/home/urls.py
@@ -1,6 +1,6 @@
 from xml.etree.ElementInclude import include
 from django.urls import path
-from .views import FetchUser,FetchBookDetails,FetchBooks,PostBook,RegisterUser,Authenticate,MyBooks,displayChat,SearchBook,postChat,EditBook,getUsersChat,FetchSimilarBook
+from .views import FetchUser,FetchBookDetails,FetchBooks,getWishlist,PostBook,RegisterUser,Wishlist,Profile,Authenticate,MyBooks,displayChat,SearchBook,postChat,EditBook,getUsersChat,similarBooks
 from rest_framework import routers
 
 
@@ -9,7 +9,6 @@ urlpatterns = [
     path('users/',FetchUser.as_view()),
     path('books/',FetchBooks.as_view()),
     path('bookdetails/',FetchBookDetails.as_view()),
-    path('similarbooks/',FetchSimilarBook.as_view()),
     path('posts/', PostBook.as_view()),
     path('register/',RegisterUser.as_view()),
     path('authenticate/',Authenticate.as_view()),
@@ -19,4 +18,8 @@ urlpatterns = [
     path('postchat/',postChat.as_view()),
     path('editbook/',EditBook.as_view()),
     path('getuserschat/',getUsersChat.as_view()),
+    path('similarbooks/',similarBooks.as_view()),
+    path('profile/',Profile.as_view()),
+    path('wish/',Wishlist.as_view()),
+    path('getwish/',getWishlist.as_view()),
 ]
